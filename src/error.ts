be76e31354ab4a5ea6329fdde23239811739ee30
Mischa Holz <me@mhlz.de>
2021-09-30 14:58:39 +0200

    Add linting step and fix linting

--- src/error.ts
+++ src/error.ts
@@ -2,8 +2,6 @@
  * error will throw an error with the given message, if any
  * @param msg - The error message
  */
-export function error(
-    msg?: string
-): never {
-    throw new Error(msg)
+export function error(msg?: string): never {
+  throw new Error(msg)
 }
