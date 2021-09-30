c87000c7d379c59aa452e653bb612c0e50a6a3c5
Mischa Holz <me@mhlz.de>
2021-09-30 14:50:09 +0200

    Add deno collections to Array as extensions

---
+++ src/collections.ts
@@ -0,0 +1,141 @@
+import * as collections from "@opencreek/deno-std-collections"
+import { apply, extendProtoype } from "./extendPrototype"
+
+type Collections = typeof collections
+
+function extendArray<
+  KEY extends {
+    [P in keyof Collections]: Collections[P] extends (
+      first: any[],
+      ...args: any
+    ) => any
+      ? P
+      : never
+  }[keyof Collections]
+>(key: KEY): void {
+  extendProtoype(Array, apply(collections[key]), key)
+}
+
+extendArray("associateBy")
+extendArray("associateWith")
+extendArray("chunk")
+extendArray("distinct")
+extendArray("distinctBy")
+extendArray("dropLastWhile")
+extendArray("dropWhile")
+extendArray("findLast")
+extendArray("findLastIndex")
+extendArray("findSingle")
+extendArray("firstNotNullishOf")
+extendArray("groupBy")
+extendArray("intersect")
+extendArray("mapNotNullish")
+extendArray("maxBy")
+extendArray("maxOf")
+extendArray("maxWith")
+extendArray("minBy")
+extendArray("minOf")
+extendArray("minWith")
+extendArray("partition")
+extendArray("permutations")
+extendArray("runningReduce")
+extendArray("sample")
+extendArray("slidingWindows")
+extendArray("sortBy")
+extendArray("sumOf")
+extendArray("takeLastWhile")
+extendArray("takeWhile")
+extendArray("takeWhile")
+extendArray("union")
+extendArray("unzip")
+extendArray("withoutAll")
+extendArray("zip")
+
+type PairSplit<T> = T extends [infer F, infer L] ? [F[], L[]] : never
+
+declare global {
+  interface Array<T extends string> {
+    associateWith<U>(selector: (key: string) => U): Record<string, U>
+  }
+
+  interface Array<T> {
+    associateBy(selector: (el: T) => string): Record<string, T>
+    chunk(size: number): T[][]
+    distinct(): T[]
+    distinctBy<D>(selector: (el: T) => D): T[]
+    dropLastWhile(predicate: (el: T) => boolean): T[]
+    dropWhile(predicate: (el: T) => boolean): T[]
+    findLast(predicate: (el: T) => boolean): T | undefined
+    findLastIndex(predicate: (el: T) => boolean): number | undefined
+    findSingle(predicate: (el: T) => boolean): T | undefined
+    firstNotNullishOf<O>(
+      selector: (item: T) => O | undefined | null
+    ): NonNullable<O> | undefined
+    groupBy(selector: (el: T) => string): Record<string, T[]>
+    intersect(...arrays: (readonly T[])[]): T[]
+    mapNotNullish<O>(transformer: (el: T) => O): NonNullable<O>[]
+    maxBy(
+      selector:
+        | ((el: T) => number)
+        | ((el: T) => string)
+        | ((el: T) => bigint)
+        | ((el: T) => Date)
+    ): T | undefined
+    maxBy(selector: (el: T) => string): T | undefined
+    maxBy(selector: (el: T) => bigint): T | undefined
+    maxBy(selector: (el: T) => number): T | undefined
+    maxBy(selector: (el: T) => Date): T | undefined
+    maxOf<S extends ((el: T) => number) | ((el: T) => bigint)>(
+      selector: S
+    ): ReturnType<S> | undefined
+    maxOf(selector: (el: T) => bigint): bigint | undefined
+    maxOf(selector: (el: T) => number): number | undefined
+    maxWith(comparator: (a: T, b: T) => number): T | undefined
+    minBy(selector: (el: T) => number): T | undefined
+    minBy(selector: (el: T) => string): T | undefined
+    minBy(selector: (el: T) => bigint): T | undefined
+    minBy(selector: (el: T) => Date): T | undefined
+    minBy(
+      selector:
+        | ((el: T) => number)
+        | ((el: T) => string)
+        | ((el: T) => bigint)
+        | ((el: T) => Date)
+    ): T | undefined
+    minOf(selector: (el: T) => bigint): bigint | undefined
+    minOf<S extends ((el: T) => number) | ((el: T) => bigint)>(
+      selector: S
+    ): ReturnType<S> | undefined
+    minOf(selector: (el: T) => number): number | undefined
+    minWith(comparator: (a: T, b: T) => number): T | undefined
+    partition(predicate: (el: T) => boolean): [T[], T[]]
+    permutations(): T[][]
+    runningReduce<O>(
+      reducer: (accumulator: O, current: T) => O,
+      initialValue: O
+    ): O[]
+    sample(): T | undefined
+    slidingWindows(
+      size: number,
+      { step, partial }: { step: number; partial: boolean }
+    ): T[][]
+    sortBy(
+      selector:
+        | ((el: T) => number)
+        | ((el: T) => string)
+        | ((el: T) => bigint)
+        | ((el: T) => Date)
+    ): T[]
+    sortBy(selector: (el: T) => Date): T[]
+    sortBy(selector: (el: T) => bigint): T[]
+    sortBy(selector: (el: T) => string): T[]
+    sortBy(selector: (el: T) => number): T[]
+    sumOf(selector: (el: T) => number): number
+    takeLastWhile(predicate: (el: T) => boolean): T[]
+    takeWhile(predicate: (el: T) => boolean): T[]
+    union(...arrays: (readonly T[])[]): T[]
+    unzip(): PairSplit<T>
+    withoutAll(values: readonly T[]): T[]
+    zip<U>(withArray: readonly U[]): [T, U][]
+  }
+}
