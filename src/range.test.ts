83580884ed822552bf7687415c936f865d4983e9
Mischa Holz <me@mhlz.de>
2022-02-11 16:02:16 +0100

    Allow range to produce negative ranges

--- src/range.test.ts
+++ src/range.test.ts
@@ -10,3 +10,17 @@ test("range should produce more difficult ranges", (t) => {
   t.deepEqual(range(3, 8, 2), [3, 5, 7])
   t.deepEqual(range(3, 8, 0.5), [3, 3.5, 4, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5])
 })
+
+test("range should produce negative ranges", (t) => {
+  t.deepEqual(range(6, 3), [6, 5, 4])
+  t.deepEqual(range(6, 1, 2), [6, 4, 2])
+  t.deepEqual(range(6, 1, -2), [6, 4, 2])
+  t.deepEqual(
+    range(8, 3, -0.5),
+    [3.5, 4, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0].reverse()
+  )
+  t.deepEqual(
+    range(8, 3, 0.5),
+    [3.5, 4, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0].reverse()
+  )
+})
