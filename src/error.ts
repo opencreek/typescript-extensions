885d1bef8c67f27f56684e652b70dc4c1e7d0752
Mischa Holz <me@mhlz.de>
2022-12-07 10:07:26 +0100

    Add string formatting functions
    Also add string formatting to error() finally

--- src/error.ts
+++ src/error.ts
@@ -1,7 +1,10 @@
+import { format } from "./strings"
+
 /**
  * error will throw an error with the given message, if any
  * @param msg - The error message
  */
-export function error(msg?: string): never {
-  throw new Error(msg)
+export function error(msg?: string, ...args: any | undefined | null): never {
+  const str = format(msg ?? "", ...args)
+  throw new Error(str)
 }
